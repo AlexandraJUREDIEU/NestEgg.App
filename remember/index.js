54c2874a655c31526e9a376dea6ce3c351e3f678
Romain672 <romain.meese@hotmail.fr>
2024-03-19 15:51:49 +0100

    ✨ newFeature: display nav mouse enter & leave
    Lorsque la souris entre dans la nav, le menu s'affiche.
    Lorsque la souris sort de la nav, le menu se cache.

--- remember/index.js
+++ remember/index.js
@@ -11,9 +11,20 @@ var swiper2 = new Swiper(".mySwiper", {
 });
 
 
+/**
+ * Montrer la nav quand la souris est dans celle-ci
+ */
+document.querySelector("nav picture").addEventListener("mouseenter", displayScrollingMenu)
+function displayScrollingMenu() {
+    //console.log("+");
+    document.querySelector("nav ul").style.display = "flex";
+}
 
-
-
+document.querySelector("nav").addEventListener("mouseleave", stopDisplayScrollingMenu)
+function stopDisplayScrollingMenu () {
+    //console.log("-");
+    document.querySelector("nav ul").style.display = "none";
+}
 /**
  * Ajoute du blur quand on affiche la Connexion ou l'Inscription
  */
