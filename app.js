d04bb2f878c8905d624a96e811cb66dff901c0e2
Romain672 <romain.meese@hotmail.fr>
2024-03-22 16:11:25 +0100

    aligns/spaces/orders

--- app.js
+++ app.js
@@ -24,40 +24,38 @@ let activeTab = "default"; //non utilisé pour le moment
 
 /**
  *  RESPONSIVITÉ
- * 
+ *
  *  1.Fonction pour ajuster l'affichage de la Navbar en fonction de la largeur de l'écran
- * 
- * 
-  */  
-  function adjustNavDisplay() {
-    // Si la largeur est supérieur à 980px
-    if (window.innerWidth > 980) {
-      hamburgerMenuIcon.style.display = "none";
-      navPrimary.style.display = "flex";
-      mainNav.classList.remove("nav-primary");
-      mainNav.classList.add("nav-secondary");
-      //cas particulier où l'utlisateur serait <980, ouvrirait le menu (avec l'oiseau), puis passerait >980 ce qui chanve le menu en haut mais il faut enlever l'oiseau manuellement
-      imgOiseauMenu.style.display = "none";
-    } else {
-      // Si la largeur est inférieur à 980px
-      hamburgerMenuIcon.style.display = "flex";
-      navPrimary.style.display = "none";
-      mainNav.classList.add("nav-primary");
-      mainNav.classList.remove("nav-secondary");
-    }
-    // Écouter les changements de taille de la fenêtre
-    window.addEventListener("resize", adjustNavDisplay);
-    
+ *
+ *
+ */
+function adjustNavDisplay() {
+  // Si la largeur est supérieur à 980px
+  if (window.innerWidth > 980) {
+    hamburgerMenuIcon.style.display = "none";
+    navPrimary.style.display = "flex";
+    mainNav.classList.remove("nav-primary");
+    mainNav.classList.add("nav-secondary");
+    //cas particulier où l'utlisateur serait <980, ouvrirait le menu (avec l'oiseau), puis passerait >980 ce qui chanve le menu en haut mais il faut enlever l'oiseau manuellement
+    imgOiseauMenu.style.display = "none";
+  } else {
+    // Si la largeur est inférieur à 980px
+    hamburgerMenuIcon.style.display = "flex";
+    navPrimary.style.display = "none";
+    mainNav.classList.add("nav-primary");
+    mainNav.classList.remove("nav-secondary");
   }
-  // Appel initial pour configurer l'affichage basé sur la largeur actuelle
-  adjustNavDisplay();
+  // Écouter les changements de taille de la fenêtre
+  window.addEventListener("resize", adjustNavDisplay);
+}
+// Appel initial pour configurer l'affichage basé sur la largeur actuelle
+adjustNavDisplay();
 
-
- /**
+/**
  * MENU DÉROULANT
  * Fonction de basculement du menu déroulant
- * 
-  */ 
+ *
+ */
 // Fonction pour ouvrir le menu
 function openScrollingMenu() {
   navPrimary.style.display = "flex";
@@ -74,28 +72,20 @@ function closeScrollingMenu() {
 }
 // Fonction de basculement du menu
 function toggleScrollingMenu() {
-  if (navPrimary.style.display === "flex" || popUpAuth.style.display === "flex" || inscription.style.display === "flex") {
+  if (
+    navPrimary.style.display === "flex" ||
+    popUpAuth.style.display === "flex" ||
+    inscription.style.display === "flex"
+  ) {
     closeMenu();
   } else {
     openScrollingMenu();
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 /** POP-UP AUTHENTIFICATION */
 //Ouvre le menu Authentification
-function openMenuAuthentification () {
+function openMenuAuthentification() {
   closeScrollingMenu();
   desactiveScroll();
   openAuthentification();
@@ -104,7 +94,7 @@ function openMenuAuthentification () {
   adjustNavDisplay();
 }
 //Ouvre le menu Inscription
-function openMenuInscription () {
+function openMenuInscription() {
   closeScrollingMenu();
   desactiveScroll();
   openAuthentification();
@@ -114,7 +104,7 @@ function openMenuInscription () {
   adjustNavDisplay();
 }
 //Ferme le menu
-function closeMenu () {
+function closeMenu() {
   closeScrollingMenu();
   activeScroll();
   closeAuthentification();
@@ -124,78 +114,66 @@ function closeMenu () {
   adjustNavDisplay();
 }
 // Affiche la partie Authentification du menu
-function openAuthentification () {
+function openAuthentification() {
   popUpAuth.style.display = "flex";
 }
 // Ferme la partie Authentification du menu
-function closeAuthentification () {
+function closeAuthentification() {
   popUpAuth.style.display = "none";
 }
 // ré-Active le scroll sur toute la page
-function activeScroll () {
+function activeScroll() {
   scroller.style.overflowY = "scroll";
 }
 // Désactive le scroll sur toute la page
-function desactiveScroll () {
+function desactiveScroll() {
   scroller.style.overflowY = "hidden";
 }
 // CONNEXION
 // Affiche la partie Connexion du menu
-function openConnexion () {
+function openConnexion() {
   connexion.style.display = "flex";
   typeConnexion.innerHTML = "Connexion";
   activeTab = "connexion";
 }
 // Ferme la partie Connexion du menu
-function closeConnexion () {
+function closeConnexion() {
   connexion.style.display = "none";
 }
 // Affiche la partie Inscription du menu
-function openInscription () {
+function openInscription() {
   inscription.style.display = "flex";
   typeConnexion.innerHTML = "Inscription";
 }
 // Ferme la partie Inscription du menu
-function closeInscription () {
+function closeInscription() {
   inscription.style.display = "none";
 }
 
-
-
-
-
-
-
-
-
-
-
 // Ajout de l'écouteur d'événements pour le menu hamburger
 hamburgerMenuIcon.addEventListener("click", toggleScrollingMenu);
- 
+
 // Fermer le menu lorsqu'on clique dans un lien de la nav ()
 for (let link of navLinks) {
   link.addEventListener("click", closeScrollingMenu);
-  for (b of btnConnect){
+  for (b of btnConnect) {
     b.addEventListener("click", closeScrollingMenu);
   }
 }
 
 // Ouvre la partie Inscription du menu
-for (b of btnSignIn){
-  b.addEventListener("click",openMenuInscription);
+for (b of btnSignIn) {
+  b.addEventListener("click", openMenuInscription);
 }
 
 // Ouvre la partie Authentification du menu
-for (b of btnConnect){
-  b.addEventListener("click",openMenuAuthentification);
+for (b of btnConnect) {
+  b.addEventListener("click", openMenuAuthentification);
 }
 
 // Ferme le menu
 for (c of closePopUp) {
   c.addEventListener("click", closeMenu);
 }
-
-
 
 // A classer
