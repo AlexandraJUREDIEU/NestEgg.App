d517483c1fd3859ed959318c55568819e7179b42
Romain672 <romain.meese@hotmail.fr>
2024-03-28 15:24:10 +0100

    💄: add style header
    Ajout du style pour le header

--- src/composants/Header.jsx
+++ src/composants/Header.jsx
@@ -5,7 +5,12 @@ import styled from 'styled-components';
 import { NavLink } from "react-router-dom";
 
 
-const StyleButton = styled.header`
+const StyleHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.75em 1em 0.75em 0em;
+    z-index: 100;
 `;
 
 
@@ -19,11 +24,13 @@ function Header(props) {
   //Return (qu'est ce qu'il affche)
   return (<>
     <nav>
-        <NavLink to={{pathname:`./${props.page1}`}}>{props.page1}</NavLink> - 
-        <NavLink to={{pathname:`./${props.page2}`}}>{props.page2}</NavLink> - 
-        <NavLink to={{pathname:`./${props.page3}`}}>{props.page3}</NavLink> - 
-        <NavLink to={{pathname:`./${props.page4}`}}>{props.page4}</NavLink> - 
-        <NavLink to={{pathname:`./${props.page5}`}}>{props.page5}</NavLink> - 
+        <StyleHeader>
+            <NavLink to={{pathname:`./${props.page1}`}}>{props.page1}</NavLink>
+            <NavLink to={{pathname:`./${props.page2}`}}>{props.page2}</NavLink>
+            <NavLink to={{pathname:`./${props.page3}`}}>{props.page3}</NavLink>
+            <NavLink to={{pathname:`./${props.page4}`}}>{props.page4}</NavLink>
+            <NavLink to={{pathname:`./${props.page5}`}}>{props.page5}</NavLink>
+        </StyleHeader>
     </nav>
     </>);
 }
