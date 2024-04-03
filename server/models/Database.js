6da894b333aa6c0788081c7c4826965e3e53baf8
Sofien <sofien.tebourbi@gmail.com>
2024-04-03 12:31:55 +0200

    simple database class / models directory add

---
+++ server/models/Database.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+
+// Database class
+const database =  class Database {
+  constructor(host, user, password, database) {
+    this.pool = mysql.createPool({
+          host: host,
+            user: user,
+            password: password,
+            database: database,
+        }).promise();
+    }
+
+    // Query function
+  async userQuery(query) {
+    try {
+      const [rows] = await this.pool.query(query);
+      return rows;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  }
+}
+
