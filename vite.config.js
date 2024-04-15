84dca92057264f39e67cb7cd67e982eaf1ca0948
Alexandra J. <158613483+AlexandraJUREDIEU@users.noreply.github.com>
2024-04-15 09:26:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0' // Écoute sur toutes les adresses IPv4 de la machine
+  }
+
 })
