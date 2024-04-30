8caa95fc8a8eedf4d57e1e4379840e809b9bdc5c
Sofien <sofien.tebourbi@gmail.com>
2024-04-30 19:26:52 +0200

    update test routes

--- server/routes/test.js
+++ server/routes/test.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 // Import the services
 const transactionService = require('../services/transactionService.js');
+const bankAccountService = require('../services/bankAccountService.js');
 const budgetService = require('../services/budgetService.js');
 
 // Define the routes
 router.get('/', (req, res) => {
-	res.send('Hello transactions!');
+    res.send('Hello transactions!');
 })
 
 router.get('/transactions', transactionService.getAllTransactions);
 router.get('/budget', budgetService.getBudgetByUser);
+router.get('/fixedCharges', bankAccountService.getFixedCharges);
 
 module.exports = router;
