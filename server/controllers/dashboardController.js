ba884c87b028d1c0c3316f957fbbeefa4de7aefc
Romain672 <romain.meese@hotmail.fr>
2024-05-03 17:34:19 +0200

    :construction: Try of links front/back

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -7,7 +7,7 @@ const transactionService = require('../services/transactionService.js');
 const bankAccountService = require('../services/bankAccountService.js');
 const budgetService = require('../services/budgetService.js');
 const auth = require('../controllers/authController.js');
-
+//const frontDashboard = require('../../src/Dashboard.jsx');
 
 // Define the routes
 router.get('/', (req, res) => {
@@ -19,10 +19,28 @@ router.get('/transactions', transactionService.getAllTransactions);
 router.get('/budget', budgetService.getBudgetByUser);
 router.get('/fixedCharges', bankAccountService.getFixedCharges);
 router.post('/connect', auth.login_post);
-router.get('/getnbrtransactions', async (req, res) => {
+/*router.get('/getnbrtransactions', async (req, res) => {
     try {
         const response = await axios.get('http://localhost:8000/dashboard/transactions');
         res.json("Il y a " + response.data[0].transactions.length + " transactions.");
+    }
+    catch {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});*/
+
+
+
+
+
+
+//router.get('/manouvelleroute', frontDashboard.displayTest);
+
+router.get('/manouvelleroute', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:8000/dashboard/transactions');
+        res.json(response.data);
     }
     catch {
         console.error(error);
@@ -33,15 +51,28 @@ router.get('/getnbrtransactions', async (req, res) => {
 
 
 
+/*
+
 router.get('/example', async (req, res) => {
     try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        res.json(response.data);
+        
+        // Modification des données
+        const modifiedData = response.data.map(item => {
+            return {
+                userId: item.userId,
+                id: item.id,
+                title: item.title.toUpperCase(), // Exemple de modification
+                body: item.body
+            };
+        });
+
+        res.json(modifiedData);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
     }
-});
+});*/
 
 
 
