7303fb6d5f02a9566131c2cfe0cf604bc8b02fea
Romain672 <romain.meese@hotmail.fr>
2024-05-08 19:35:35 +0200

    ✨ Active of profil + 2sections of profil +
    - le li actif est celui qui correspond au  nom de la page
    - ajout des deux sections de la page profil

--- src/MyAccount.jsx
+++ src/MyAccount.jsx
@@ -3,19 +3,64 @@ import HeaderProfile from './assets/layout/HeaderProfil.jsx';
 import styled from "styled-components";
 //Styles
 const Style = styled.header`
+  /*Vue globale*/
   width:100wh;
   height:100vh;
+  .section{
+    border-radius:50px;
+    background-color: rgba(255, 255, 255, 0.5);
+    padding:0 5em;
+    padding-top:1em;
+  }
+
+  /*Vue mobile*/
+  @media screen and (max-width: 1000px) {
+    .sections {
+      position:relative;
+      width:100%;
+      height:90%;
+      padding:1em;
+      display:flex;
+      flex-direction:column;
+      justify-content:space-around;
+    }
+    .section1{
+      height:20%;
+    }
+    .section2 {
+      height:60%;
+    }
+  }
+  
+  /*Vue desktop*/
+  @media screen and (min-width: 1000px) {
+    .sections {
+      position:relative;
+      left:20%;
+      width:80%;
+      height:100%;
+      padding:1em;
+      display:flex;
+      flex-direction:column;
+      justify-content:space-around;
+    }
+    .section1{
+      height:20%;
+    }
+    .section2 {
+      height:70%;
+    }
+  }
 `;
 
 
 
 //Fonctions
 const links = [
-  {to:"/bank-account" , text:"Compte bancaire"},
-  {to:"/income" , text:"Revenus"},
-  {to:"/monthly-bills" , text:"Charges fixes"},
-  {to:"/setting" , text:"Paramètre"},
-  {to:"/dashboard" , text:"Tableau de bord"}, //A retirer
+  {to:"/my-account" , text:"Placeholder my account active", imgsrc:"icons-menu-hamburger.png"},
+  {to:"/bank-account" , text:"Compte bancaire", imgsrc:"icons-menu-hamburger.png"},
+  {to:"/income" , text:"Revenus", imgsrc:"icons-menu-hamburger.png"},
+  {to:"/monthly-bills" , text:"Charges fixes", imgsrc:"icons-menu-hamburger.png"},
   ];
 
 function MyAccount() {
@@ -26,7 +71,10 @@ function MyAccount() {
       <>
         <Style>
         <HeaderProfile links={links}></HeaderProfile>
-          Mon profil:
+        <div className="sections">
+          <section className="section section1">section1</section>
+          <section className="section section2">section2</section>
+        </div>
         </Style>
       </>
     )
