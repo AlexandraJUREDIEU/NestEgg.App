386dba989c068c74f86f12b0f2594aa2fbf62ac2
Romain672 <romain.meese@hotmail.fr>
2024-05-15 11:45:56 +0200

    :construction: Routes of my-account
    Créations des différentes pages my-account

--- src/MyAccount.jsx
+++ src/MyAccount.jsx
@@ -1,17 +1,96 @@
 
-import React, { useState } from 'react';
+import styled from "styled-components";
+import React from 'react';
 
-import {Route, Switch} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Profil from "./pages/myAccount/Profil.jsx";
 import Compte from "./pages/myAccount/Compte.jsx";
+import Ressources from "./pages/myAccount/Ressources.jsx";
+import ChargesFixes from "./pages/myAccount/ChargesFixes.jsx";
+
+
+//Styles
+const Style = styled.div`
+  /*Vue globale*/
+  width:100%;
+  height:100vh;
+  section{
+    border-radius:3em;
+  }
+  img{
+    width:0.8em;
+    height:0.8em;
+  }
+
+  /*Vue mobile*/
+  @media screen and (max-width: 1000px) {
+    .sections {
+      position:relative;
+      width:100%;
+      height:90%;
+      padding:1em;
+      display:flex;
+      flex-direction:column;
+      justify-content:space-around;
+    }
+    .section1{
+      height:20%;
+    }
+    .section2 {
+      height:60%;
+    }
+  }
+  
+  /*Vue desktop*/
+  @media screen and (min-width: 1000px) {
+    .sections {
+      position:relative;
+      left:20%;
+      width:80%;
+      height:100%;
+      padding:1em;
+      display:flex;
+      flex-direction:column;
+      justify-content:space-around;
+    }
+    .section1{
+      height:20%;
+    }
+    .section2 {
+      height:70%;
+    }
+  }
+`;
+
+
+const SectionWrapper = styled.section`
+  position: relative;
+  height: 100vh;
+  left:0;
+`;
+
+
+//Fonctions
 
 function MyAccount() {
+    //State
+    //Comportement
+    //Render
+
+    /*
+          <SectionWrapper>
+          </SectionWrapper>
+    */
     return (
       <>
-            <Switch>
-              <Route path="/profil" component={Profil} />
-              <Route path="/compte" component={Compte} />
-            </Switch>
+        <Style>
+          <Routes>
+            <Route path="/profil" element={<Profil />} />
+            <Route path="/compte" element={<Compte />} />
+            <Route path="/ressources" element={<Ressources />} />
+            <Route path="/chargesFixes" element={<ChargesFixes />} />
+          </Routes>
+        </Style>
       </>
     )
   }
